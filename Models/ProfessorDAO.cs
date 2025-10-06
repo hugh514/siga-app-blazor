9482cd3559e7a9e29443a6aa3b5290f0dd4c9dba
Hugo Santos <139011452+hugh514@users.noreply.github.com>
2025-10-06 00:22:55 -0400

    feat: adicionar exclusão do professor

--- Models/ProfessorDAO.cs
+++ Models/ProfessorDAO.cs
@@ -2,6 +2,8 @@
 using SigaApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace SigaApp.Models
 {
@@ -18,25 +20,27 @@ namespace SigaApp.Models
         {
             try
             {
-                var comando = _conexao.CreateCommand(@"
+                using (var comando = _conexao.CreateCommand(@"
                     INSERT INTO professor 
                     VALUES (null, @_nome, @_cpf, @_email, @_telefone, @_status, @_disciplina, @_turmas, @_especialidade, @_dataCadastro)
-                ");
+                "))
+                {
+                    comando.Parameters.AddWithValue("@_nome", professor.Nome);
+                    comando.Parameters.AddWithValue("@_cpf", professor.Cpf);
+                    comando.Parameters.AddWithValue("@_email", professor.Email ?? "");
+                    comando.Parameters.AddWithValue("@_telefone", professor.Telefone ?? "");
+                    comando.Parameters.AddWithValue("@_status", professor.Status ?? "Ativo");
+                    comando.Parameters.AddWithValue("@_disciplina", professor.DisciplinaPrincipal ?? "");
+                    comando.Parameters.AddWithValue("@_turmas", professor.TurmasVinculadas ?? "");
+                    comando.Parameters.AddWithValue("@_especialidade", professor.Especialidade ?? "");
+                    comando.Parameters.AddWithValue("@_dataCadastro", professor.DataCadastro);
 
-                comando.Parameters.AddWithValue("@_nome", professor.Nome);
-                comando.Parameters.AddWithValue("@_cpf", professor.Cpf);
-                comando.Parameters.AddWithValue("@_email", professor.Email ?? "");
-                comando.Parameters.AddWithValue("@_telefone", professor.Telefone ?? "");
-                comando.Parameters.AddWithValue("@_status", professor.Status ?? "Ativo");
-                comando.Parameters.AddWithValue("@_disciplina", professor.DisciplinaPrincipal ?? "");
-                comando.Parameters.AddWithValue("@_turmas", professor.TurmasVinculadas ?? "");
-                comando.Parameters.AddWithValue("@_especialidade", professor.Especialidade ?? "");
-                comando.Parameters.AddWithValue("@_dataCadastro", professor.DataCadastro);
-
-                comando.ExecuteNonQuery();
+                    comando.ExecuteNonQuery();
+                }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Console.WriteLine($"Erro ao inserir professor: {ex.Message}");
                 throw;
             }
         }
@@ -45,29 +49,51 @@ namespace SigaApp.Models
         {
             var lista = new List<Professor>();
 
-            var comando = _conexao.CreateCommand("SELECT * FROM professor");
-            var leitor = comando.ExecuteReader();
-
-            while (leitor.Read())
+            using (var comando = _conexao.CreateCommand("SELECT * FROM professor"))
+            using (var leitor = comando.ExecuteReader())
             {
-                var professor = new Professor
+                while (leitor.Read())
                 {
-                    Id = leitor.GetInt32("id_prof"),
-                    Nome = leitor.GetString("nome_prof"),
-                    Cpf = leitor.GetString("cpf_prof"),
-                    Email = leitor.IsDBNull(leitor.GetOrdinal("email_prof")) ? "" : leitor.GetString("email_prof"),
-                    Telefone = leitor.IsDBNull(leitor.GetOrdinal("telefone_prof")) ? "" : leitor.GetString("telefone_prof"),
-                    Status = leitor.IsDBNull(leitor.GetOrdinal("status_prof")) ? "Ativo" : leitor.GetString("status_prof"),
-                    DisciplinaPrincipal = leitor.IsDBNull(leitor.GetOrdinal("disciplina_principal_prof")) ? "" : leitor.GetString("disciplina_principal_prof"),
-                    TurmasVinculadas = leitor.IsDBNull(leitor.GetOrdinal("turmas_vinculadas_prof")) ? "" : leitor.GetString("turmas_vinculadas_prof"),
-                    Especialidade = leitor.IsDBNull(leitor.GetOrdinal("especialidade_prof")) ? "" : leitor.GetString("especialidade_prof"),
-                    DataCadastro = leitor.GetDateTime("data_cadastro_prof")
-                };
+                    var professor = new Professor
+                    {
+                        Id = leitor.GetInt32("id_prof"),
+                        Nome = leitor.GetString("nome_prof"),
+                        Cpf = leitor.GetString("cpf_prof"),
+                        Email = leitor.IsDBNull(leitor.GetOrdinal("email_prof")) ? "" : leitor.GetString("email_prof"),
+                        Telefone = leitor.IsDBNull(leitor.GetOrdinal("telefone_prof")) ? "" : leitor.GetString("telefone_prof"),
+                        Status = leitor.IsDBNull(leitor.GetOrdinal("status_prof")) ? "Ativo" : leitor.GetString("status_prof"),
+                        DisciplinaPrincipal = leitor.IsDBNull(leitor.GetOrdinal("disciplina_principal_prof")) ? "" : leitor.GetString("disciplina_principal_prof"),
+                        TurmasVinculadas = leitor.IsDBNull(leitor.GetOrdinal("turmas_vinculadas_prof")) ? "" : leitor.GetString("turmas_vinculadas_prof"),
+                        Especialidade = leitor.IsDBNull(leitor.GetOrdinal("especialidade_prof")) ? "" : leitor.GetString("especialidade_prof"),
+                        DataCadastro = leitor.GetDateTime("data_cadastro_prof")
+                    };
 
-                lista.Add(professor);
+                    lista.Add(professor);
+                }
             }
 
             return lista;
+        }
+
+        public void Excluir(int id)
+        {
+            try
+            {
+                using (var comando = _conexao.CreateCommand(@"
+                    DELETE FROM professor
+                    WHERE id_prof = @_id
+                "))
+                {
+                    comando.Parameters.AddWithValue("@_id", id);
+                    comando.ExecuteNonQuery();
+                    Console.WriteLine($"Professor ID {id} excluído com sucesso!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao excluir professor: {ex.Message}");
+                throw;
+            }
         }
     }
 }
